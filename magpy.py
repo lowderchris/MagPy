11fec3ed695bdac2dea47ff4941ff2cc11d9c10d
Chris Lowder <lowder.chris@icloud.com>
2022-06-28 09:25:35 -0600

    Basic outline to begin code porting

---
+++ magpy.py
@@ -0,0 +1,14 @@
+# MagPy
+# Solar Magnetic Field Visualization Toolkit
+
+# Outline
+
+# Overview, installation, and brief documentation for Blender
+
+# Description of standard input data format
+
+# Description of use
+
+# Config files or define inline paths / parameters
+
+# MagPy class definition
