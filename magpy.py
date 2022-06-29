18ec323e9a9714e4d3a4fc25b3e70a6532cc386f
Chris Lowder <lowder.chris@icloud.com>
2022-06-29 10:30:26 -0600

    Sketching out some ideas

--- magpy.py
+++ magpy.py
@@ -4,11 +4,30 @@
 # Outline
 
 # Overview, installation, and brief documentation for Blender
-
 # Description of standard input data format
-
 # Description of use
 
 # Config files or define inline paths / parameters
 
 # MagPy class definition
+
+import matplotlib.image
+import numpy
+import sys
+import numpy as np
+import sunpy.io
+from PIL import Image
+
+import bpy
+from mathutils import Vector
+
+class MagPy:
+    """
+    
+    """
+
+    def read():
+
+    def preview():
+
+    def render():
